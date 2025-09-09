ea6b9186070b5b691014f3fc0ad1a7b007b1bc28
Camily Nogueira da Silva <2401657@SALAS.AULAS>
2025-09-08 22:22:00 -0300

    feet: adicionar autenticação

--- src/Infrastructure/Model/activation_code.py
+++ src/Infrastructure/Model/activation_code.py
@@ -1,11 +1,18 @@
 from src.config.data_base import db
+from sqlalchemy.orm import relationship
 
 class ActivationCode(db.Model):
-    __tablename__ = "activation_codes"
+    __tablename__ = 'activation_codes'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    code = db.Column(db.String(10), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(4), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     used = db.Column(db.Boolean, default=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
-    user = db.relationship("User", back_populates="activation_codes")
+    user = db.relationship(
+        'User',
+        back_populates='activation_codes'
+    )
+    
+    def __repr__(self):
+        return f'<ActivationCode {self.code}>'
