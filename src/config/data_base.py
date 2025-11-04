a8a0731b3eece3288c04d1058fa26e942f58adab
JulianaMzz <juliana24muniz@gmail.com>
2025-11-03 21:41:00 -0300

    fix: Ajuste no root

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -9,7 +9,7 @@ def init_db(app):
     """
     Inicializa a base de dados com o app Flask e o SQLAlchemy.
     """
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:123456@localhost:3306/market_management'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Jujuba345!@localhost:3306/marketsystem'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     bcrypt.init_app(app)
