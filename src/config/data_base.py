3be9a817bbb33a5b90952179049146d4a25b0ecc
JulianaMzz <juliana24muniz@gmail.com>
2025-11-09 03:49:15 -0300

    feat: ajuste para funcionamento de deploy

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -1,3 +1,4 @@
+import os # 👈 IMPORTANTE: Precisamos do módulo 'os'
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -7,9 +8,27 @@ bcrypt = Bcrypt()
 
 def init_db(app):
     """
-    Inicializa a base de dados com o app Flask e o SQLAlchemy.
+    Inicializa a base de dados lendo a DATABASE_URL do ambiente (Railway/Render).
+    Se a variável não for encontrada, usa a URL local como fallback.
     """
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Jujuba345!@localhost:3306/marketsystem'
+    
+    # 1. TENTA OBTER A URL DA VARIÁVEL DE AMBIENTE (RENDER/RAILWAY)
+    database_url = os.environ.get("DATABASE_URL")
+    
+    # 2. Configurações da aplicação
+    if database_url:
+        # Usa a URL externa (Railway)
+        app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+        print("INFO: Usando DATABASE_URL do ambiente para conexão remota.")
+    else:
+        
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Jujuba345!@localhost:3306/marketsystem'
+        print("WARNING: DATABASE_URL não encontrada, usando localhost.")
+        
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+   
+    app.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET_KEY", "SUA_CHAVE_PADRAO_LOCAL")
+
     db.init_app(app)
     bcrypt.init_app(app)
